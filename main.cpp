3ae476e07ea236c29aad2aec07dd9ed2e14314f1
AndriiYar <andreydekabrin@gmail.com>
2026-05-16 23:50:16 +0300

    fix check for saved words

--- main.cpp
+++ main.cpp
@@ -18,15 +18,17 @@ void showMenu() {
     }
 
 bool wordExists(const std::vector<strtWords>& words, const std::string& word) 
-{
-    for (const strtWords& savedWord : words) {
+    {
+    for (const strtWords& savedWord : words) 
+    {
         if (savedWord.engWord == word) 
-        {
+            {
             return true;
-        }
+            }
     }
     return false;
-}
+    }
+  
 void addWords(std::vector<strtWords>& words) 
 {
     std::string word;
@@ -34,20 +36,20 @@ void addWords(std::vector<strtWords>& words)
     std::cin >> word;
     while (word != "exit")
     {
-    if (word.empty())
-        {
-        std::cout << "Word cannot be empty!" << std::endl;
-        continue;
-        }
-    if (wordExists(words, word)) {
+        if (wordExists(words, word)) {
         std::cout << "This word already adds!" << std::endl;
         std ::cout << "Enter a English word or 'exit' for exit): ";
         std::cin >> word;
         continue;
         }
+    if (word.empty())
+        {
+        std::cout << "Word cannot be empty!" << std::endl;
+        continue;
+        }
     strtWords newWord;
     newWord.engWord = word;
-    std::cout << "Enter a Ukrainian word: ";
+    std::cout << "Enter a Ukrainian translation: ";
     std::cin >> newWord.uaWord;
 
     if (newWord.uaWord.empty())
@@ -56,26 +58,26 @@ void addWords(std::vector<strtWords>& words)
         std::cin >> word;
         continue;
         }
-      words.push_back(newWord);
-        std::cout << "Word added!" << std::endl;
-        
-        std::cout << "Enter a English nextword or type 'exit' to return to menu : ";
-        std::cin >> word;
+    words.push_back(newWord);
+    std::cout << "Word added!" << std::endl;   
+    std::cout << "Enter a English next word or type 'exit' to return to menu : ";
+    std::cin >> word;
     }
 }
 
-void showWords(const std ::vector<strtWords>& words) {
-    if(words.empty())
-        {
-            std::cout << "Your vocabulary is empty!" << std::endl; 
-        }
+void showWords(const std ::vector<strtWords>& words) 
+    {
+        if(words.empty())
+            {
+                std::cout << "Your vocabulary is empty!" << std::endl; 
+            }
         else 
-        {std::cout<< "Your vocabulary: " << std::endl;
-              for(const strtWords& savedWord : words)
-                {
-                 std::cout << savedWord.engWord << " - " << savedWord.uaWord << std::endl;
-                }
-        }
+            {std::cout<< "Your vocabulary: " << std::endl;
+                for(const strtWords& savedWord : words)
+                    {
+                        std::cout << savedWord.engWord << " - " << savedWord.uaWord << std::endl;
+                    }
+            }
     }
 
 void saveWordstoCSV (const std::vector<strtWords>& words)
@@ -87,6 +89,14 @@ void saveWordstoCSV (const std::vector<strtWords>& words)
         }   
     }
 
+
+void removetrailingSpaces(std::string& str) 
+    {
+        while (!str.empty() && str.back() == ' ') 
+        {
+            str.pop_back();
+        }
+    }
 void loadwordsfromCSV(std::vector<strtWords>& words) 
     {
         std::ifstream file("vocabularystore.csv");
@@ -100,6 +110,7 @@ void loadwordsfromCSV(std::vector<strtWords>& words)
 
         while (std ::getline (file, engWord, '-') && std::getline(file, uaWord))
         {
+            removetrailingSpaces(engWord);
             strtWords word;
             word.engWord = engWord;
             word.uaWord = uaWord;
